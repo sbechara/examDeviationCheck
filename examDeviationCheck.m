dde53a3e22c4370ff6ce60a9e450a3b05f76d09d
Samuel Bechara <sbechara@gmail.com>
2019-04-25 10:35:59 -0600

    Fixed a commenting problem in the script

--- examDeviationCheck.m
+++ examDeviationCheck.m
@@ -4,24 +4,24 @@
 % Created on: 19 April 2018
 % By: Samuel Bechara, PhD
 %
-Description:
-  This script is intended to highlight students who may have had a single
-  bad test. Use of this script and the decisions that arise from using it
-  are at the discretion of Dr. Bechara. There are no guarantees that if
-  you are flagged that you will get an exam grade change. Flagged means
-  Dr. Bechara will consider your homework scores, participation,
-  discussion posts. If it appears considerable effort has been made in
-  these areas, Dr. Bechara MAY consider to replace the lowest exam score
-  with the average of the other three.
-
-Filter Explanations
-  First Filter:
-      Filter out students that have two D's or F's or have one zero
-  Second Filter:
-      For all other students, "drop" their lowest score and calculate the
-      std and mean of the other three exams. If they are tightly bunched,
-      average to a 89 or better, AND the student doesn't already have an
-      A average, flag for checkup.
+% Description:
+%   This script is intended to highlight students who may have had a single
+%   bad test. Use of this script and the decisions that arise from using it
+%   are at the discretion of Dr. Bechara. There are no guarantees that if
+%   you are flagged that you will get an exam grade change. Flagged means
+%   Dr. Bechara will consider your homework scores, participation,
+%   discussion posts. If it appears considerable effort has been made in
+%   these areas, Dr. Bechara MAY consider to replace the lowest exam score
+%   with the average of the other three.
+% 
+% Filter Explanations
+%   First Filter:
+%       Filter out students that have two D's or F's or have one zero
+%   Second Filter:
+%       For all other students, "drop" their lowest score and calculate the
+%       std and mean of the other three exams. If they are tightly bunched,
+%       average to a 89 or better, AND the student doesn't already have an
+%       A average, flag for checkup.
 % ---------------------------------------------------------------------- %
 
 clear
